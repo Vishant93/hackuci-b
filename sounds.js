41c9208b262c2d2ac71b38279aee0400e73c9c26
vikilla <vishantk@gmail.com>
2014-05-24 20:29:25 -0700

    6 audio clips added
    stay based

--- sounds.js
+++ sounds.js
@@ -4,7 +4,13 @@ function PlayRandomSound()
 				  "sound/i live for you.mp3",
 				  "sound/i value friends.mp3",
 				  "sound/i wanna say i love you.mp3",
-				  "sound/its not about how you look on the outside.mp3"
+				  "sound/its not about how you look on the outside.mp3",
+				  "sound/bless your soul.mp3",
+				  "sound/frownupsidedown.mp3",
+				  "sound/loveyou.mp3",
+				  "sound/staypositive.mp3",
+				  "sound/swagmax.mp3",
+				  "sound/touching.mp3",
 				  ];
 	var index  = Math.floor(Math.random()*sounds.length);
 	console.log(index);
